a3a2287c0ad34523cce4f598e21ebf9497645b93
[dyad] <paulinko98@gmail.com>
2025-10-30 00:54:17 +0100

    [dyad] Switched to HashRouter for GitHub Pages compatibility - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // Changed BrowserRouter to HashRouter
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { I18nextProvider } from "react-i18next"; // New import
-import i18n from "@/lib/i18n"; // New import
+import { I18nextProvider } from "react-i18next";
+import i18n from "@/lib/i18n";
 
 const queryClient = new QueryClient();
 
@@ -15,14 +15,14 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <I18nextProvider i18n={i18n}> {/* New: Wrap with I18nextProvider */}
-        <BrowserRouter>
+      <I18nextProvider i18n={i18n}>
+        <HashRouter> {/* Changed to HashRouter */}
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </I18nextProvider>
     </TooltipProvider>
   </QueryClientProvider>
